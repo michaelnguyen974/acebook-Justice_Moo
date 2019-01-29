69c39cf7bdbb1a6a2242e2deb50046d46015aead
Tom Russell <TR115251@hotmail.co.uk>
2019-01-29 14:51:16 +0000

    test: first feature test for sign-up "

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "sign up", type: :feature do
+  scenario "user can sign up" do
+    visit 'welcome/index'
+    click_button "signup"
+    fill_in "email", with: "test@test.com"
+    fill_in "password", with: "testpassword"
+    click_button 'submit'
+    expect(page).to have_content('Welcome test@test.com')
+  end
+end
