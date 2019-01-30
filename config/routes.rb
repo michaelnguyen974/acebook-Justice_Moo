3887849650fdd64ee7962065ff9c2f9c4be6394e
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-30 11:14:54 +0000

    feature:sign up not working but about to edit to match sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  root 'welcome#index'
+
   get 'welcome/signup'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  # root 'welcome#index'
 
   resources :posts
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,18 +3,17 @@ require 'rails_helper'
 describe "sign up", type: :feature do
   scenario "user can sign up" do
     visit 'welcome/index'
-    expect(current_path).to eq('/welcome/index')
+    expect(current_path).to eq('welcome/index')
   end
 end
 
-# describe "sign up", type: :feature do
-#   scenario "user can sign up" do
-#     visit 'welcome/index'
-#     click_button "signup"
-#     # expect(current_path).to eq('signup')
-#     fill_in "email", with: "test@test.com"
-#     fill_in "password", with: "testpassword"
-#     click_button 'submit'
-#     expect(page).to have_content('Welcome test@test.com')
-#   end
-# end
+describe "sign up", type: :feature do
+  scenario "user can sign up" do
+    visit 'welcome/index'
+    click_button "signup"
+    fill_in "email", with: "test@test.com"
+    fill_in "password", with: "testpassword"
+    click_button 'submit'
+    expect(page).to have_content('Welcome test@test.com')
+  end
+end
