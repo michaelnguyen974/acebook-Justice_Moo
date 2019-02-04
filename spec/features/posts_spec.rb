1db03181f5e10a69a3964673247b4cfd65575b1b
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-04 16:42:16 +0000

    test: failing delete post feature test written

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -21,4 +21,12 @@ feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+    scenario "Can delete their posts" do
+    go_homepage
+    sign_up
+    make_a_post("Hello wad")
+    expect(page).to have_content("Hello wad")
+    click_on 'Delete post'
+    expect(page).to not_have_content("Hello wad")
+  end
 end
--- spec/webhelper.rb
+++ spec/webhelper.rb
@@ -26,3 +26,10 @@ def sign_up_short_password
   fill_in "user[password_confirmation]", with: "passw"
   click_button 'Sign up'
 end
+
+def make_a_post(message)
+  visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: message
+    click_button "Submit"
+end
